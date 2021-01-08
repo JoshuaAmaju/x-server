2383a9b88ba539e1ed2b57602a2135b7a2cfb6d8
Joshua Amaju <princejosh0007@gmail.com>
2021-01-08 19:10:49 +0100

    remove stale code

--- example/index.ts
+++ example/index.ts
@@ -21,65 +21,6 @@ app.use(express.static(path.join(__dirname, "../../public")));
 app.use("/api", BlogApi);
 app.use(Blog);
 
-const config = {
-  routes: {
-    "/": {
-      on: {
-        GET: "home",
-      },
-    },
-    "/create": {
-      on: {
-        GET: (req: Request, res: Response) => {
-          console.log(req.originalUrl);
-          res.send("create");
-        },
-      },
-    },
-    "/:id": {
-      on: {
-        GET: ({ params, originalUrl }: Request, res: Response) => {
-          console.log(originalUrl);
-          res.send("item " + params.id);
-        },
-      },
-    },
-  },
-  actions: {
-    home: (req: Request, res: Response) => {
-      console.log(req.originalUrl);
-      res.send("home");
-    },
-  },
-};
-
-function create(config: any, mapping?: any) {
-  const router = Router();
-  const { actions, routes } = config;
-
-  Object.keys(routes).forEach((path) => {
-    const { on } = routes[path];
-    const _path = mapping?.[path] ?? path;
-
-    Object.keys(on).forEach(() => {
-      router.all(_path, (req, res, next) => {
-        const action = on[req.method];
-        const _action = typeof action === "string" ? actions[action] : action;
-        _action(req, res);
-      });
-    });
-  });
-
-  return router;
-}
-
-// const routerA = create(config);
-
-// const routerB = create(config);
-
-// app.use("/api", routerB);
-// app.use(routerA);
-
 app.listen(8080, () => {
   console.log("Server started");
 });
